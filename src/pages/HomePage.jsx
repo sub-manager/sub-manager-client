3f60faa5bccea62a88ecdbaae15f9b4bcfa3b413
khalid Sharahili <kaled.sharahili@gmail.com>
2023-06-19 12:13:38 +0300

    Forms completed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
 import Modal from "../components/forms/Modal";
-import { BsFolderPlus } from "react-icons/bs";
-import { BsFolder } from "react-icons/bs";
+import { BsFolderPlus, BsCalendar2DateFill, BsFolder } from "react-icons/bs";
 import { MdAddCircleOutline } from "react-icons/md";
 import { RiEditBoxFill } from "react-icons/ri";
 import { TiDelete } from "react-icons/ti";
 import { FaVrCardboard } from "react-icons/fa";
-import { BsCalendar2DateFill } from "react-icons/bs";
-
-import { useActionData } from "react-router-dom";
 
 const HomePage = () => {
   const [showModal, setShowModal] = useState(false);
   const [showSubFormModal, setShowSubFormModal] = useState(false);
+  const [showUbdataSubForm, setShowUbdataSubForm] = useState(false);
+  const [showDeleteSubAlert, setShowDeleteSubAlert] = useState(false);
+
   return (
     <>
       <div className="grid lg:grid-cols-3 xl:cols-2 gap h-screen mt-20 p-20">
@@ -133,12 +132,12 @@ const HomePage = () => {
                 <div className="font mt-3 text-center">
                   <div className="flex gap-4">
                     <div>
-                      <button>
+                      <button onClick={() => setShowUbdataSubForm(true)}>
                         <RiEditBoxFill size={"30"} color="#5e9ba1" />
                       </button>
                     </div>
                     <div>
-                      <button>
+                      <button onClick={() => setShowDeleteSubAlert(true)}>
                         <TiDelete size={"30"} color="#ef4444" />
                       </button>
                     </div>
@@ -175,11 +174,15 @@ const HomePage = () => {
                   <div className="flex gap-4">
                     <div>
                       <button>
-                        <RiEditBoxFill size={"30"} color="#5e9ba1" />
+                        <RiEditBoxFill
+                          onClick={() => setShowUbdataSubForm(true)}
+                          size={"30"}
+                          color="#5e9ba1"
+                        />
                       </button>
                     </div>
                     <div>
-                      <button>
+                      <button onClick={() => setShowDeleteSubAlert(true)}>
                         <TiDelete size={"30"} color="#ef4444" />
                       </button>
                     </div>
@@ -194,7 +197,7 @@ const HomePage = () => {
         <Modal isVisible={showModal} onClose={() => setShowModal(false)}>
           <div className="">
             <div className="p-8 lg:w-full mx-auto">
-              <div className="bg-gray-100 rounded-b-lg py-12 px-4 lg:px-24">
+              <div className="bg-gray-300 rounded-b-lg py-12 px-4 lg:px-24">
                 <p className="text-center text-sm text-gray-500 font-bold">
                   Create Folder
                 </p>
@@ -229,7 +232,7 @@ const HomePage = () => {
       >
         <div className="">
           <div className="p-8 lg:w-full mx-auto">
-            <div className="bg-gray-100 rounded-b-lg py-12 px-4 lg:px-24">
+            <div className="bg-gray-300 rounded-b-lg py-12 px-4 lg:px-24">
               <p className="text-center text-sm text-gray-500 font-bold">
                 Create New Subscription
               </p>
@@ -274,7 +277,6 @@ const HomePage = () => {
                       <option value="yearly">Yearly</option>
                     </select>
                   </div>
-                  {/* RADIO */}
                   <div className="mt-2 flex gap-4 items-center">
                     <label className="block mb-2 text-sm font-medium text-[#232323] ">
                       renewable
@@ -287,46 +289,114 @@ const HomePage = () => {
                     </select>
                   </div>
                 </div>
-                {/* <div className="mt-2">
-                  <label className="w-full py-4 ml-2 text-sm font-medium text-black">
-                    Renewal automatically
-                  </label>
-                </div>
-                <div className="mt-4">
-                  <div className="flex items-center pl-4 mb-2 border border-gray-200 rounded dark:border-gray-700">
-                    <input
-                      id="bordered-radio-1"
-                      type="radio"
-                      value=""
-                      name="bordered-radio"
-                      className="w-4 h-4  text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label className="w-full py-4 ml-2 text-sm font-medium text-black">
-                      Yes
-                    </label>
-                  </div>
-                  <div className="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
-                    <input
-                      checked
-                      id="bordered-radio-2"
-                      type="radio"
-                      value=""
-                      name="bordered-radio"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label className="w-full py-4 ml-2 text-sm font-medium text-black">
-                      No
-                    </label>
-                  </div>
-                </div> */}
-                {/* END RADIO */}
-
                 <div className="flex items-center justify-center mt-8">
                   <button className="text-white py-2 px-4 uppercase rounded bg-[#5e9ba1] hover:bg-black shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
                     Add Subscription
                   </button>
                 </div>
               </form>
+            </div>
+          </div>
+        </div>
+      </Modal>
+      {/* Modal of update subscription information */}
+      <Modal
+        isVisible={showUbdataSubForm}
+        onClose={() => setShowUbdataSubForm(false)}
+      >
+        <div className="bg-gray-300">
+          <div className="p-8 lg:w-full mx-auto">
+            <div className=" rounded-b-lg py-12 px-4 lg:px-24">
+              <p className="text-center text-sm text-gray-500 font-bold">
+                Update Subscription information
+              </p>
+              <form className="mt-6">
+                <div className="relative mb-4">
+                  <input
+                    className="appearance-none border pl-12 border-gray-100 shadow-sm focus:shadow-md focus:placeholder-gray-600  transition  rounded-md w-full py-3 text-gray-600 leading-tight focus:outline-none focus:ring-gray-600 focus:shadow-outline"
+                    id="username"
+                    type="text"
+                    placeholder="Provider Name"
+                  />
+                  <div className="absolute left-0 inset-y-0 flex items-center">
+                    <FaVrCardboard className="h-7 w-7 ml-3 text-gray-400 p-1" />
+                  </div>
+                </div>
+
+                <div className="relative">
+                  <input
+                    className="appearance-none border pl-12 border-gray-100 shadow-sm focus:shadow-md focus:placeholder-gray-600  transition  rounded-md w-full py-3 text-gray-600 leading-tight focus:outline-none focus:ring-gray-600 focus:shadow-outline"
+                    id="username"
+                    type="date"
+                    placeholder="dd-mm-yyyy"
+                  />
+                  <div className="absolute left-0 inset-y-0 flex items-center">
+                    <BsCalendar2DateFill className="h-7 w-7 ml-3 text-gray-400 p-1" />
+                  </div>
+                </div>
+
+                <div className="flex gap-4 items-center">
+                  <div className="mt-2 flex gap-4 items-center">
+                    <label className="block mb-2 text-sm font-medium text-[#232323] ">
+                      Cycle
+                    </label>
+                    <select
+                      id="countries"
+                      className="bg-gray-50 border border-gray-300 text-[#232323] text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-[#232323] dark:text-[#232323] dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    >
+                      <option value="weekly" selected>
+                        Weekly
+                      </option>
+                      <option value="monthly">Monthly</option>
+                      <option value="yearly">Yearly</option>
+                    </select>
+                  </div>
+                  <div className="mt-2 flex gap-4 items-center">
+                    <label className="block mb-2 text-sm font-medium text-[#232323] ">
+                      renewable
+                    </label>
+                    <select className="bg-gray-50 border border-gray-300 text-[#232323] text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-[#232323] dark:text-[#232323] dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                      <option value="yes" selected>
+                        yes
+                      </option>
+                      <option value="no">no</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="flex items-center justify-center mt-8">
+                  <button className="text-white py-2 px-4 uppercase rounded bg-[#5e9ba1] hover:bg-black shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+                    Update Subscription
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </Modal>
+
+      {/* Alert Delete Message */}
+      <Modal
+        isVisible={showDeleteSubAlert}
+        onClose={() => setShowDeleteSubAlert(false)}
+      >
+        <div className="">
+          <div className="p-8 lg:w-full mx-auto">
+            <div className=" rounded-b-lg py-12 px-4 lg:px-24">
+              <p className="text-center text-sm text-gray-500 font-bold">
+                Are you sure you want to delete this subscription
+              </p>
+              <div className="flex gap-4">
+                <div className="flex items-center justify-center mt-8">
+                  <button className="text-white py-2 px-4 uppercase rounded bg-[#ef4444] hover:bg-[#ef4444] shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+                    Delete Subscription
+                  </button>
+                </div>
+                <div className="flex items-center justify-center mt-8">
+                  <button className="text-white py-2 px-4 uppercase rounded bg-[#5e9ba1] hover:bg-black shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+                    Cancel
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
