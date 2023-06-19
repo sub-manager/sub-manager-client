b44db01f1412ad23c39066c872a2650d11dc9e75
Mosab albishi <90303281+musabalbishi@users.noreply.github.com>
2023-06-19 12:26:57 +0300

    commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,6 @@ import Modal from "../components/forms/Modal";
 import { BsFolderPlus, BsFolder } from "react-icons/bs";
 import { MdAddCircleOutline } from "react-icons/md";
 
-
 import Axios from "axios";
 import Subscription from "../components/Subscription";
 import SubscriptionForm from "../components/forms/SubscriptionForm";
@@ -26,7 +25,6 @@ const HomePage = () => {
   }, []);
   const [showUbdataSubForm, setShowUbdataSubForm] = useState(false);
   const [showDeleteSubAlert, setShowDeleteSubAlert] = useState(false);
-
 
   return (
     <>
@@ -129,12 +127,8 @@ const HomePage = () => {
                 date={sub.date}
               />
             ))}
-           
-
-              </div>
-            </div>
-
-        
+          </div>
+        </div>
 
         {/* MODAL OF CREATE FOLDER */}
         <Modal isVisible={showModal} onClose={() => setShowModal(false)}>
@@ -180,14 +174,7 @@ const HomePage = () => {
                 Create New Subscription
               </p>
 
-              {/* form here */}
-              <SubscriptionForm
-                onChange={() => {}}
-                // onChange={() => {}}
-                onClick={() => {}}
-              />
-
-              
+              <SubscriptionForm />
             </div>
           </div>
         </div>
@@ -203,65 +190,7 @@ const HomePage = () => {
               <p className="text-center text-sm text-gray-500 font-bold">
                 Update Subscription information
               </p>
-              <form className="mt-6">
-                <div className="relative mb-4">
-                  <input
-                    className="appearance-none border pl-12 border-gray-100 shadow-sm focus:shadow-md focus:placeholder-gray-600  transition  rounded-md w-full py-3 text-gray-600 leading-tight focus:outline-none focus:ring-gray-600 focus:shadow-outline"
-                    id="username"
-                    type="text"
-                    placeholder="Provider Name"
-                  />
-                  <div className="absolute left-0 inset-y-0 flex items-center">
-                    <FaVrCardboard className="h-7 w-7 ml-3 text-gray-400 p-1" />
-                  </div>
-                </div>
-
-                <div className="relative">
-                  <input
-                    className="appearance-none border pl-12 border-gray-100 shadow-sm focus:shadow-md focus:placeholder-gray-600  transition  rounded-md w-full py-3 text-gray-600 leading-tight focus:outline-none focus:ring-gray-600 focus:shadow-outline"
-                    id="username"
-                    type="date"
-                    placeholder="dd-mm-yyyy"
-                  />
-                  <div className="absolute left-0 inset-y-0 flex items-center">
-                    <BsCalendar2DateFill className="h-7 w-7 ml-3 text-gray-400 p-1" />
-                  </div>
-                </div>
-
-                <div className="flex gap-4 items-center">
-                  <div className="mt-2 flex gap-4 items-center">
-                    <label className="block mb-2 text-sm font-medium text-[#232323] ">
-                      Cycle
-                    </label>
-                    <select
-                      id="countries"
-                      className="bg-gray-50 border border-gray-300 text-[#232323] text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-[#232323] dark:text-[#232323] dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    >
-                      <option value="weekly" selected>
-                        Weekly
-                      </option>
-                      <option value="monthly">Monthly</option>
-                      <option value="yearly">Yearly</option>
-                    </select>
-                  </div>
-                  <div className="mt-2 flex gap-4 items-center">
-                    <label className="block mb-2 text-sm font-medium text-[#232323] ">
-                      renewable
-                    </label>
-                    <select className="bg-gray-50 border border-gray-300 text-[#232323] text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-[#232323] dark:text-[#232323] dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                      <option value="yes" selected>
-                        yes
-                      </option>
-                      <option value="no">no</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="flex items-center justify-center mt-8">
-                  <button className="text-white py-2 px-4 uppercase rounded bg-[#5e9ba1] hover:bg-black shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
-                    Update Subscription
-                  </button>
-                </div>
-              </form>
+              <SubscriptionForm />
             </div>
           </div>
         </div>
